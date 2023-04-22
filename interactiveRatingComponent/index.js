8336f76fbcc13b4b2d48a32507027b6fa465c2c4
Sophie <sophiercowley@hotmail.com>
2023-04-22 21:31:24 +0100

    first attempt with js code

--- interactiveRatingComponent/index.js
+++ interactiveRatingComponent/index.js
@@ -1,20 +1,9 @@
-function submitFeedback (id) {
+const rating = document.getElementById("rating")
+const rates = document.querySelectorAll(".score")
 
-    if (id === 1){
-        return "You have selected 1 out of 5"
-    } else if (id === 2){
-        return  "You have selected 2 out of 5"
-    } else if (id === 3){
-        return "You have selected 3 out of 5"
-    } else if (id === 4){
-        return "You have selected 4 out of 5"
-    } else if (id === 5){
-        return "You have selected 5 out of 5"
-    } else {
-        return "You have not provided a rating"
-    }
-}
 
-submitFeedback ()
-
-document.getElementsByClassName("score").addEventListener("click", submitFeedback)
+rates.forEach((rate)=> {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    })
+})
