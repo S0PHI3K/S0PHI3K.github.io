4f91f7fbc0ed6ef27b9016f9712e887b5e058a7b
Sophie <sophiercowley@hotmail.com>
2023-04-23 22:36:01 +0100

    basic functionality completed

--- interactiveRatingComponent/index.js
+++ interactiveRatingComponent/index.js
@@ -4,6 +4,16 @@ const rates = document.querySelectorAll(".score")
 
 rates.forEach((rate)=> {
     rate.addEventListener("click", () => {
+        // rate.style.backgroundColor = 'hsl(25, 97%, 53%)'
         rating.innerHTML = rate.innerHTML
     })
+})
+
+const cardContainer = document.querySelector('.card-container')
+const thanksContainer = document.querySelector('.thank-you-container')
+const submit = document.querySelector('.submit-button')
+
+submit.addEventListener("click", () => {
+    cardContainer.style.display = 'none';
+    thanksContainer.style.display = 'block';
 })
